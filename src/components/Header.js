2ac229a8d75540203525d143bb63bdffcbe53362
Ashu Sharma <ashusharma7644@gmail.com>
2024-03-18 18:15:09 +0530

    add files

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     
     <div className='main'>
       <div className='inner-main'>
-      <div className='logo'><h2>Arvind Sharma</h2></div>
+      <div className='logo'><h2><span className='logo-first'>Arvind</span> Sharma</h2></div>
       <div className='menus'>
       <ul>
           <li><NavLink to='/'>Home</NavLink></li>
@@ -18,6 +18,37 @@ const Header = () => {
         
       </div>
     </div>
+
+
+
+
+
+    {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <NavLink class="navbar-brand" to="/">Navbar</NavLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <NavLink class="nav-link active" aria-current="page" to="/">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink class="nav-link" to="/about">About Us</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink class="nav-link" to="/contact">Contact Us</NavLink>
+        </li>
+       
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> */}
     
   </>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import logo from './images/main-image.jpg'
+import { NavLink } from 'react-router-dom'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='main-home-page'>
+        <div className='inner-home inner-col'>
+          <div className='content-home inner-content'>
+            <h1>Hello, Welcome</h1>
+            <p>I'm Arvind Sharma, I'm a Frontend Developer. My goal is to write clean and deliver awesome final product. I am very persistent and detiled oriented. My skill include Web design , Wordpress , Html, Css ,etc.</p>
+            <button className='btn'> <NavLink to="/about">Read More</NavLink></button>
+          </div>
+          <div className='image-home'>
+          <img src={logo} alt="React Image" />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
+
+
+
+
 
 export default Home
