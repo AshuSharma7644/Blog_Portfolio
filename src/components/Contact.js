00720f4fff9bf579b5e1f6b459ddb3fea6c7a227
Ashu Sharma <ashusharma7644@gmail.com>
2024-03-19 20:49:36 +0530

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+
+      <div>
+        <div className='container'>
+        <div class="row">
+  <div class="col">
+    <input type="text" className="form-control" placeholder="First name" aria-label="First name"/>
+  </div>
+  <div class="col">
+    <input type="text" className="form-control" placeholder="Last name" aria-label="Last name"/>
+  </div>
+</div>
+
+<div className='row'>
+<div className="col">
+    <input type="text" className="form-control" placeholder="Email"/>
+  </div>
+  <div className="col">
+    <input type="number" className="form-control" placeholder="Mobile Number"/>
+  </div>
+</div>
+        </div>
+      </div>
+    </>
   )
 }
 
